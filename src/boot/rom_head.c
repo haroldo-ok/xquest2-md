7e5da911d2337f5e211f64a7021f8abdebba908c
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2026-04-08 10:40:13 -0300

    Bump version to 0.0.1

--- src/boot/rom_head.c
+++ src/boot/rom_head.c
@@ -10,8 +10,8 @@ const ROMHeader rom_header = {
     "SEGA MEGA DRIVE ",
 #endif
     "(C)SGDK 2021    ",
-    "SAMPLE PROGRAM                                  ",
-    "SAMPLE PROGRAM                                  ",
+    "XQUEST 2 v0.0.1                                 ",
+    "XQUEST 2 v0.0.1                                 ",
     "GM 00000000-00",
     0x000,
     "JD              ",
@@ -28,6 +28,6 @@ const ROMHeader rom_header = {
     0x00200000,
     0x0020FFFF,
     "            ",
-    "DEMONSTRATION PROGRAM                   ",
+    "XQUEST 2 v0.0.1                         ",
     "JUE             "
 };

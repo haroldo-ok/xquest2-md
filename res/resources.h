ff6231ebd81101db7239626718099495c13c937b
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2026-04-10 06:20:36 -0300

    Bump version to 0.0.3

--- res/resources.h
+++ res/resources.h
@@ -10,23 +10,19 @@ extern const u8 sfx_5[36352];
 extern const u8 sfx_6[7424];
 extern const u8 sfx_7[512];
 extern const u8 sfx_8[16896];
-extern const Palette pal_title;
 extern const Palette pal_bg;
 extern const Palette pal_active;
 extern const Palette pal_collect;
 extern const Palette pal_enemy;
+extern const Palette pal_title;
 extern const Palette pal_enemy2;
 extern const TileSet bg_tileset;
 extern const TileSet xquest_font;
-
-/* Player & Objects */
 extern const SpriteDefinition spr_ship;
 extern const SpriteDefinition spr_mine;
 extern const SpriteDefinition spr_enemy_mine;
 extern const SpriteDefinition spr_bullet_player;
 extern const SpriteDefinition spr_smartbomb;
-
-/* Gems / Gate / Portals / Powerups */
 extern const SpriteDefinition spr_gem;
 extern const SpriteDefinition spr_supercrystal;
 extern const SpriteDefinition spr_gate;
@@ -38,17 +34,11 @@ extern const SpriteDefinition spr_corner_tl;
 extern const SpriteDefinition spr_corner_tr;
 extern const SpriteDefinition spr_corner_bl;
 extern const SpriteDefinition spr_corner_br;
-
-/* FX */
 extern const SpriteDefinition spr_explosion;
-
-/* Enemy bullets */
 extern const SpriteDefinition spr_bullet_green;
 extern const SpriteDefinition spr_bullet_yellow;
 extern const SpriteDefinition spr_bullet_purple;
 extern const SpriteDefinition spr_bullet_buckshot;
-
-/* Enemies */
 extern const SpriteDefinition spr_grunger;
 extern const SpriteDefinition spr_zippo;
 extern const SpriteDefinition spr_zinger;
@@ -65,7 +55,6 @@ extern const SpriteDefinition spr_buckshot;
 extern const SpriteDefinition spr_cluster;
 extern const SpriteDefinition spr_sticktight;
 extern const SpriteDefinition spr_repulsor;
-
 extern const Image img_title;
 
 #endif // _RES_RESOURCES_H_
